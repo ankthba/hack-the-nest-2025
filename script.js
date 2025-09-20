6185a19822e57c1e373a582d9fcc6faa327ca4c2
lauren14-08 <laurenkim2008@gmail.com>
2025-09-20 16:02:29 -0400

    Website

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
 // JavaScript code goes here
 
+//intro stuff
 document.addEventListener('DOMContentLoaded', function() {
-    // Your code here
+    const h2 = document.getElementById('reveal-h2');
+    function revealH2OnScroll() {
+        if (window.scrollY > 400) {
+            h2.style.opacity = 1;
+            window.removeEventListener('scroll', revealH2OnScroll);
+        }
+    }
+    window.addEventListener('scroll', revealH2OnScroll);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const p = document.getElementById('reveal-p');
+    function revealpOnScroll() {
+        if (window.scrollY > 400) {
+            p.style.opacity = 1;
+            window.removeEventListener('scroll', revealpOnScroll);
+        }
+    }
+    window.addEventListener('scroll', revealpOnScroll);
+});
+
+//problem & solution stuff
+document.addEventListener('DOMContentLoaded', function() {
+    const h2 = document.getElementById('reveal-title');
+    function revealtitleOnScroll() {
+        if (window.scrollY > 800) {
+            h2.style.opacity = 1;
+            window.removeEventListener('scroll', revealtitleOnScroll);
+        }
+    }
+    window.addEventListener('scroll', revealtitleOnScroll);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const p = document.getElementById('reveal-text');
+    function revealtextOnScroll() {
+        if (window.scrollY > 800) {
+            p.style.opacity = 1;
+            window.removeEventListener('scroll', revealtextOnScroll);
+        }
+    }
+    window.addEventListener('scroll', revealtextOnScroll);
 });
